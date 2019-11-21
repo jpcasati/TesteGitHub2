c38a816c86d01b1a1658a0f971a403ac1022c77d
JPCASATI <jpcasati@usp.br>
2019-11-21 09:59:26 -0300

    Adicionado o botao de saudacao na tela inicial.

--- app/src/main/java/br/usp/testegithub/MainActivity.kt
+++ app/src/main/java/br/usp/testegithub/MainActivity.kt
@@ -2,11 +2,19 @@ package br.usp.testegithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var ola = btnOla
+
+        ola.setOnClickListener {
+            Toast.makeText(this, "Ola Visitante!", Toast.LENGTH_LONG).show()
+        }
     }
 }
